e7cb3b8a9c420018a09c1b81a7051bd93eb60d82
mwelford2 <83231271+mwelford2@users.noreply.github.com>
2026-03-27 16:02:01 -0400

    Cleaned up data merge script

--- src/manual_id_adder.py
+++ src/manual_id_adder.py
@@ -1,8 +1,6 @@
 import csv
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-import urllib.parse
 
 driver = webdriver.Chrome()
 
@@ -11,16 +9,6 @@ def parse_url_to_id(url):
     split = url.split('/')
     i = split.index('player') + 1 # id is the next item after player
     return split[i]
-
-def get_final_url(query):
-    url = "https://www.google.com/search?btnI=1&q=" + urllib.parse.quote(query)
-    driver.get(url + query.replace(' ', '+'))
-
-    # Wait until we land on the sofifa page
-    WebDriverWait(driver, 10).until(
-        lambda d: "sofifa" in d.current_url and 'player' in d.current_url
-    )
-    return driver.current_url
 
 def main():
     with open('unmatched.txt', 'r') as unmatched, \
@@ -35,8 +23,7 @@ def main():
             player_position = player_data[3]
             player_birth_year = player_data[4]
 
-            query = f'{player_name} sofifa id'
-            URL = get_final_url(query)
+            URL = input("Enter sofifa url: ")
 
             try:
                 ID = parse_url_to_id(URL)
--- src/merge_player_data.py
+++ src/merge_player_data.py
@@ -1,11 +1,37 @@
+import urllib
+
 import search as search_players
 import csv
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Chrome()
 
 def format_name(name):
     first = name.split(" ")[0]
     last = name.split(" ")[-1]
     first_letter = first[0]
     return f"{first_letter}. {last}"
+
+def get_id_from_url(url):
+    split = url.split('/')
+    i = split.index('player') + 1 # id is the next item after player
+    return split[i]
+
+def get_sofifa_url(query):
+    url = "https://www.google.com/search?btnI=1&q=" + urllib.parse.quote(query)
+    driver.get(url + query.replace(' ', '+'))
+
+    # Wait until we land on the sofifa page
+    WebDriverWait(driver, 10).until(
+        lambda d: "sofifa" in d.current_url and 'player' in d.current_url
+    )
+    return driver.current_url
+
+def search_sofifa_for_id(name):
+    query = f'{name} sofifa id'
+    URL = get_sofifa_url(query)
+    return get_id_from_url(URL)
 
 def main():
     with open("../Fifa 2022 Full Player Database.csv", 'r', newline='') as all_players, \
@@ -21,29 +47,39 @@ def main():
         matched = 0
         unmatched = 0
         unmatched_names = []
+        new_row = {}
 
         # NOTE THE ORDER: id, sticker num, player name, team, position, birth year
         for wc_player in wc_players_csv:
             wc_p_name = format_name(wc_player["player"])
+            new_row['sofifa_id'] = 0
+            new_row['sticker_id'] = "PLACEHOLDER"
+            new_row['player_name'] = wc_player["player"]
+            new_row['team'] = wc_player["team"]
+            new_row['position'] = wc_player["position"]
+            new_row['birth_year'] = wc_player["birth_year"]
             for player in all_players_rows:
                 found = False
                 p_name = player["short_name"]
                 if search_players.matches_text(p_name, wc_p_name):
-                    output.writerow({
-                        "sofifa_id": player["\ufeffsofifa_id"],
-                        "sticker_id": "PLACEHOLDER",
-                        "player_name": p_name,
-                        "team": wc_player["team"],
-                        "position": wc_player["position"],
-                        "birth_year": wc_player["birth_year"]
-                    })
+                    new_row['sofifa_id'] = player["\ufeffsofifa_id"]
+                    output.writerow(new_row)
                     found = True
                     matched += 1
                     break  # Move on to next World Cup player after first match.
 
             if not found:
-                unmatched += 1
-                unmatched_names.append(f"PLACEHOLDER,{wc_player['player']},{wc_player['team']},{wc_player['position']},{wc_player['birth_year']}")
+                name = wc_player["player"]
+                print(f"{name} not found. Adding from sofifa search...")
+                try:
+                    ID = search_sofifa_for_id(name)
+                except Exception as e:
+                    unmatched_names.append(name)
+                    print(f"Search failed for {name}, error: {e}")
+                    unmatched += 1
+                    continue
+                new_row['sofifa_id'] = ID
+                output.writerow(new_row)
 
         print(f"Matched {matched} players, {unmatched} unmatched.")
         for p in unmatched_names:
